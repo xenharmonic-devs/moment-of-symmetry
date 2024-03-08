d80a6dbf0e1424b8b9b5e3ec90c2fc778fc99faf
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2024-03-08 12:45:26 +0200

    Add unit tests for the helper functions

---
+++ src/__tests__/helpers.spec.ts
@@ -0,0 +1,71 @@
+import {describe, it, expect} from 'vitest';
+import {bjorklund, modInv, bresenham, cumsum} from '../helpers';
+import {gcd, mmod} from 'xen-dev-utils';
+
+describe("Björklund's algorithm", () => {
+  it('distributes 3 beats evenly into a grid of 5', () => {
+    const result = bjorklund([[true], [true], [true], [false], [false]]);
+    expect(result.flat(Infinity)).toEqual([true, false, true, false, true]);
+  });
+
+  it('distributes 2 beats evenly into a grid of 6', () => {
+    const result = bjorklund([
+      [true],
+      [true],
+      [false],
+      [false],
+      [false],
+      [false],
+    ]);
+    expect(result.flat(Infinity)).toEqual([
+      true,
+      false,
+      false,
+      true,
+      false,
+      false,
+    ]);
+  });
+});
+
+describe('Modular inverse', () => {
+  it.each([2, 3, 4, 5, 6, 11, 79, 100])('Finds inverses mod %d', modulus => {
+    for (let i = 1; i < modulus; ++i) {
+      if (gcd(i, modulus) !== 1) {
+        expect(() => modInv(i, modulus)).toThrow();
+      } else {
+        const inverse = modInv(i, modulus);
+        expect(mmod(i * inverse, modulus)).toBe(1);
+      }
+    }
+  });
+});
+
+describe('Bresenham line algorithm', () => {
+  it('distributes 3 beats evenly into a grid of 5', () => {
+    const result = bresenham(3, 5 - 3, true, false);
+    expect(result).toEqual([true, true, false, true, false]);
+  });
+
+  it('distributes 2 beats evenly into a grid of 6', () => {
+    const result = bresenham(2, 6 - 2, true, false);
+    expect(result).toEqual([true, false, false, true, false, false]);
+  });
+
+  it('produces the brightest modes', () => {
+    for (let l = 1; l < 20; ++l) {
+      for (let s = 1; s < 10; s++) {
+        let basic = bresenham(l, s, 2, 1);
+        basic = cumsum(basic.concat(basic));
+        basic.unshift(0);
+        for (let i = 1; i < l + s; ++i) {
+          const wide = basic[i] - basic[0];
+          for (let j = 1; j < l + s; ++j) {
+            const narrowOrEqual = basic[i + j] - basic[j];
+            expect(wide).toBeGreaterThanOrEqual(narrowOrEqual);
+          }
+        }
+      }
+    }
+  });
+});
---
+++ src/helpers.ts
@@ -0,0 +1,94 @@
+/**
+ * Internal helper functions not intended to be published.
+ */
+
+import {arraysEqual, extendedEuclid, gcd} from 'xen-dev-utils';
+
+/**
+ * Distribute subsequences as evenly as possible using Björklund's algorithm.
+ */
+export function bjorklund(subsequences: any[][]) {
+  while (true) {
+    const remainder = subsequences[subsequences.length - 1];
+    const distributed: any[][] = [];
+    while (
+      subsequences.length &&
+      arraysEqual(subsequences[subsequences.length - 1], remainder)
+    ) {
+      distributed.push(subsequences.pop()!);
+    }
+    if (!subsequences.length || distributed.length <= 1) {
+      return subsequences.concat(distributed);
+    }
+    for (let i = 0; distributed.length && i < subsequences.length; ++i) {
+      subsequences[i] = subsequences[i].concat(distributed.pop());
+    }
+    subsequences = subsequences.concat(distributed);
+  }
+}
+
+// This algorithm, a variant of the Bresenham line algorithm, returns the "brightest mode" of
+// the "scale" where `first` is treated as larger than `second`.
+// It's based on following the closest approximation of the line y = b/a*x that is strictly below the line.
+export function bresenham(
+  a: number,
+  b: number,
+  first: any,
+  second: any
+): any[] {
+  const d = gcd(a, b);
+  if (d === 1) {
+    const result: boolean[] = [];
+    // `xHere` = current number of `first`, `yHere` = current number of `second`; start at (0, 0).
+    let [xHere, yHere] = [0, 0];
+    while (xHere < a || yHere < b) {
+      // If going north (taking a (0, 1) step) doesn't lead to going north of the line y = b/a*x,
+      if (a * (yHere + 1) <= b * xHere) {
+        // append `second` to `resultScale` and update the current location.
+        result.push(second);
+        yHere += 1;
+      } else {
+        // Else, append `first` and take one step to the east.
+        result.push(first);
+        xHere += 1;
+      }
+    }
+    return result;
+  } else {
+    // aLbs is a d-period MOS, so we concatenate `d` copies of the primitive MOS a/d*L b/d*s.
+    return Array(d)
+      .fill(bresenham(a / d, b / d, first, second))
+      .flat();
+  }
+}
+
+/**
+ * Find the modular inverse of a mod b, provided gcd(a,b) == 1.
+ */
+export function modInv(a: number, b: number): number {
+  const ee = extendedEuclid(a, b);
+  const {gcd, coefA} = ee;
+  if (gcd === 1) {
+    return ((coefA % b) + b) % b; // to ensure remainder is always in {0, 1, ..., b-1}
+  } else {
+    throw new Error(
+      '`a` does not have a modular inverse mod `b` since `a` and `b` are not coprime'
+    );
+  }
+}
+
+/**
+ * Cumulative sum of input array.
+ * @param array Array of steps.
+ * @returns Scale of accumulated steps.
+ */
+export function cumsum(array: number[]): number[] {
+  if (!array.length) {
+    return [];
+  }
+  const result = [array[0]];
+  for (let i = 1; i < array.length; ++i) {
+    result.push(result[i - 1] + array[i]);
+  }
+  return result;
+}
